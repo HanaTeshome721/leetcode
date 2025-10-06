431d90bdeb4d58466464ab227461882ac00dc9ba
HanaTeshome721 <hana2729tm@gmail.com>
2025-10-06 09:10:27 +0000

    Code challenge submission for question: 1929 Concatenation of Array

---
+++ 1929 Concatenation of Array/1929concatenation-of-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        # ans=[]
+        # ans.extend(nums)
+        # ans.extend(nums)
+        #ans=nums+nums
+        ans=nums*2
+        return ans
