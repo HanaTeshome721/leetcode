3864a173f420500185f0c7660e180b07cfac080c
HanaTeshome721 <hana2729tm@gmail.com>
2025-10-20 02:17:11 +0000

    Code challenge submission for question: 2671 Frequency Tracker

---
+++ 2671 Frequency Tracker/2671frequency-tracker.py
@@ -0,0 +1,27 @@
+class FrequencyTracker:
+
+    def __init__(self):
+        self.count=defaultdict(int)
+        self.frq=defaultdict(int)
+
+    def add(self, number: int) -> None:
+        self.frq[self.count[number]]-=1
+        self.count[number]+=1
+        self.frq[self.count[number]]+=1
+
+    def deleteOne(self, number: int) -> None:
+        if self.count[number] > 0:
+            self.frq[self.count[number]]-=1
+            self.count[number]-=1
+            self.frq[self.count[number]]+=1
+
+    def hasFrequency(self, frequency: int) -> bool:
+        return self.frq[frequency]>0
+        
+
+
+# Your FrequencyTracker object will be instantiated and called as such:
+# obj = FrequencyTracker()
+# obj.add(number)
+# obj.deleteOne(number)
+# param_3 = obj.hasFrequency(frequency)
