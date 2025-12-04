77cdc6bedec45a4d0281af0d589bbd959f9adc28
HanaTeshome721 <hana2729tm@gmail.com>
2025-12-04 03:06:29 +0000

    Code challenge submission for question: 71 Simplify Path

---
+++ 71 Simplify Path/71simplify-path.py
@@ -0,0 +1,14 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        path=list(filter(None,path.split('/')))
+        print(path)
+        stack=[]
+        for p in path:
+            if p=='.':
+                continue
+            elif p=="..":
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(p)
+        return '/'+'/'.join(stack)        
