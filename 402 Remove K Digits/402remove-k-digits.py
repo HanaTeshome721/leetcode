8efa98c73bfabd765e82e4f7bbb9018d147d2a88
HanaTeshome721 <hana2729tm@gmail.com>
2025-12-14 03:54:58 +0000

    Code challenge submission for question: 402 Remove K Digits

---
+++ 402 Remove K Digits/402remove-k-digits.py
@@ -0,0 +1,24 @@
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+       stack=[]
+       for n in num:
+            while stack and k>0 and stack[-1]>n:
+                stack.pop()
+                k-=1
+            stack.append(n)
+    #    stack=stack[:len(stack)-k]
+       if k:
+         stack=stack[:-k]
+
+       res=''.join(stack).lstrip('0')
+       return res if res else "0"
+ 
+
+
+
+
+
+
+
+
+    
