9bc159f630d3d6bdfb73cb5a26ca3ab9ed3fcf28
HanaTeshome721 <hana2729tm@gmail.com>
2026-02-18 03:06:18 +0000

    Code challenge submission for question: 654 Maximum Binary Tree

---
+++ 654 Maximum Binary Tree/654maximum-binary-tree.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
+
+
+        if not nums:
+            return None
+        val=max(nums)
+        i=nums.index(val)
+        root=TreeNode(val)
+        root.left=self.constructMaximumBinaryTree(nums[:i])   
+        root.right=self.constructMaximumBinaryTree(nums[i+1:])   
+        return root
+
+
+
+        # nodes = []
+        # for num in nums:
+        #     node = TreeNode(num)
+        #     while nodes and nodes[-1].val < num:
+        #         node.left = nodes.pop()
+
+        #     if nodes:
+        #         nodes[-1].right = node
+
+        #     nodes.append(node)
+
+        # return nodes[0]
+
+         
