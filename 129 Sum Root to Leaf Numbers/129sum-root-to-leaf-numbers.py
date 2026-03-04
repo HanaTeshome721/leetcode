594fa7631c2ef5954f11ac69cc29b361058d5104
HanaTeshome721 <hana2729tm@gmail.com>
2026-03-04 15:58:47 +0000

    Code challenge submission for question: 129 Sum Root to Leaf Numbers

---
+++ 129 Sum Root to Leaf Numbers/129sum-root-to-leaf-numbers.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        #  def dfs(node,s):
+        #     if not node:
+        #         return 0
+        #     s+=str(node.val)  
+        #     if not node.left and not node.right:
+        #         return int(s)
+        #     return dfs(node.left,s) + dfs(node.right,s)
+        #  return dfs(root,s="") 
+
+
+
+         def dfs(node,cur):
+            if not node:
+                return 0
+            cur= cur*10 + node.val
+            if not node.left and not node.right:
+                return cur
+            return dfs(node.left,cur) + dfs(node.right,cur)
+         return dfs(root,0)                     
+
+
+
+
+
+
+
+
+
+
