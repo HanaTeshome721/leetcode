d840afaedf28d842d8bbf084e31a6711f8fc01f1
HanaTeshome721 <hana2729tm@gmail.com>
2026-03-17 03:25:34 +0000

    Code challenge submission for question: 2139 Minimum Moves to Reach Target Score

---
+++ 2139 Minimum Moves to Reach Target Score/2139minimum-moves-to-reach-target-score.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minMoves(self, target: int, maxDoubles: int) -> int:
+      move=0
+      while target>1 and maxDoubles:
+        if target%2:
+            target-=1
+        else:
+            target//=2
+            maxDoubles-=1
+        move+=1
+      return move + target-1          
