4b8edd370bc9b001a9c669b501c9c0998d73297a
hana <117968852+HanaTeshome721@users.noreply.github.com>
2026-09-16 22:10:50 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (73.46%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -2,6 +2,6 @@ class Solution:
     def longestCommonPrefix(self, strs: list[str]) -> str:
         strs.sort()
         for i in range(len(strs[0])):
-            if  i<len(strs[-1]) and strs[0][i]!=strs[-1][i]:
+            if  strs[0][i]!=strs[-1][i]:
                 return strs[0][:i]
         return strs[0]        
